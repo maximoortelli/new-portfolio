dcb1624f4f3431b897212d12528eed968495ff8b
maximoortelli <maximoortelli@hotmail.com>
2023-09-23 23:23:50 -0300

    add files
    add menu.js
    Update style.css
    Update index.html

---
+++ menu.js
@@ -0,0 +1,25 @@
+function openMenu() {
+    document.getElementById("myMenu").style.width = "250px";
+    document.getElementById("myOverlay").style.width = "100%";
+    document.body.style.marginLeft = "0px";
+  }
+
+  function closeMenu() {
+    document.getElementById("myMenu").style.width = "0";
+    document.getElementById("myOverlay").style.width = "0";
+    document.body.style.marginLeft= "0";
+  }
+
+  /*Elimino el texto de abajo del menu bar*/
+
+/*
+  document.querySelector('.openMenu').addEventListener('click', function() {
+    document.getElementById('home-content').style.display = 'none';
+});*/
+/*
+  document.querySelector('.closeBtn').addEventListener('click', function() {
+      document.getElementById('home-content').style.display = '';
+});*/
+
+/**/
+
